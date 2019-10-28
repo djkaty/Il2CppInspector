858be5fa3edc03f5b8ca802da3309f4c4100a33a
Katy Coe <djkaty@start.no>
2019-10-28 23:23:19 +0100

    X64: Add stub code

---
+++ Il2CppInspector/Il2CppBinaryX64.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Copyright 2019 Katy Coe - http://www.hearthcode.org - http://www.djkaty.com
+
+    All rights reserved.
+*/
+
+using System;
+using System.Linq;
+
+namespace Il2CppInspector
+{
+    internal class Il2CppBinaryX64 : Il2CppBinary
+    {
+        public Il2CppBinaryX64(IFileFormatReader stream) : base(stream) { }
+        public Il2CppBinaryX64(IFileFormatReader stream, uint codeRegistration, uint metadataRegistration) : base(stream, codeRegistration, metadataRegistration) { }
+
+        protected override (ulong, ulong) ConsiderCode(IFileFormatReader image, uint loc) {
+            return (0, 0);
+        }
+    }
+}
