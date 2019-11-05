bec3528f7715e9ce677e31df6993c7993e1a175c
Katy Coe <djkaty@start.no>
2019-11-06 00:33:47 +0100

    Tests: Add Properties test

---
+++ Il2CppTests/TestSources/Properties.cs
@@ -0,0 +1,16 @@
+﻿/*
+    Copyright 2017-2019 Katy Coe - http://www.hearthcode.org - http://www.djkaty.com
+
+    All rights reserved.
+*/
+
+namespace Il2CppTests.TestSources
+{
+    internal class Test
+    {
+        private int prop1 { get; set; }
+        protected int prop2 { get; private set; }
+        protected int prop3 { private get; set; }
+        public static int prop4 { private get; set; }
+    }
+}
