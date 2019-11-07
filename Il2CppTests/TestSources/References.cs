dd1d8d91d8253f3f7a74beb091046bef2020dd37
Katy Coe <djkaty@start.no>
2019-11-07 03:27:49 +0100

    Tests: Add References test

---
+++ Il2CppTests/TestSources/References.cs
@@ -0,0 +1,29 @@
+﻿/*
+    Copyright 2017-2019 Katy Coe - http://www.hearthcode.org - http://www.djkaty.com
+
+    All rights reserved.
+*/
+
+namespace Il2CppTests.TestSources
+{
+#pragma warning disable CS0169
+    internal class Test
+    {
+        private float floatField;
+
+        public void MethodWithRefParameters(int a, ref int b, int c, ref int d) {}
+
+        // In parameters were introduced in C# 7.2
+        public void MethodWithInRefOut(in int a, ref int b, out int c) => c = 1;
+
+        public ref float MethodWithRefReturnType() => ref floatField;
+    }
+
+    // Ref structs were introduced in C# 7.2 - creates IsByRefLike attribute on type in assembly
+    // Attribute doesn't seem to be retained by IL2CPP?
+    public ref struct RefStruct
+    {
+        private int structField1;
+    }
+#pragma warning restore CS0169
+}
