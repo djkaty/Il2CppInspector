f4a1a21e9ee116b15fb93f49c47164e66561210e
Katy Coe <djkaty@start.no>
2020-02-07 03:52:08 +0100

    GUI: Load metadata file

--- Il2CppInspector.GUI/App.xaml.cs
+++ Il2CppInspector.GUI/App.xaml.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Il2CppInspector;
 
-namespace Il2CppInspector.GUI
+namespace Il2CppInspectorGUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public Metadata CurrentMetadata { get; private set; }
+
+        public Exception LastException { get; private set; }
+
+        // Attempt to load an IL2CPP metadata file
+        public Task<bool> LoadMetadataAsync(string metadataFile) =>
+            Task.Run(() => {
+                try {
+                    CurrentMetadata = new Metadata(new MemoryStream(File.ReadAllBytes(metadataFile)));
+                    return true;
+                }
+                catch (Exception ex) {
+                    LastException = ex;
+                    return false;
+                }
+            });
     }
 }
--- Il2CppInspector.GUI/MainWindow.xaml.cs
+++ Il2CppInspector.GUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,8 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using Il2CppInspector;
 
-namespace Il2CppInspector.GUI
+namespace Il2CppInspectorGUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -28,11 +31,26 @@ namespace Il2CppInspector.GUI
         /// <summary>
         /// Select global metadata file
         /// </summary>
-        private void BtnSelectMetadataFile_OnClick(object sender, RoutedEventArgs e) {
-            var openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "IL2CPP global metadata file|global-metadata.dat|All files (*.*)|*.*";
+        private async void BtnSelectMetadataFile_OnClick(object sender, RoutedEventArgs e) {
+            var app = (App) Application.Current;
+
+            var openFileDialog = new OpenFileDialog {
+                Filter = "IL2CPP global metadata file|global-metadata.dat|All files (*.*)|*.*",
+                CheckFileExists = true
+            };
+
+            btnSelectMetadataFile.Visibility = Visibility.Hidden;
+
             if (openFileDialog.ShowDialog() == true) {
-                // openFileDialog.FileName
+                // Load the metadata file
+                if (await app.LoadMetadataAsync(openFileDialog.FileName)) {
+                    // Metadata loaded successfully
+                    lblMetadataVersion.DataContext = app.CurrentMetadata;
+                }
+                else {
+                    MessageBox.Show(this, app.LastException.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    btnSelectMetadataFile.Visibility = Visibility.Visible;
+                }
             }
         }
     }
