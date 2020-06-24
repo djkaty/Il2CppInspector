a48720796cc332b649341d1b64d045fb76f1d469
Katy Coe <djkaty@users.noreply.github.com>
2020-06-24 22:18:16 +0200

    Tests: Add test stub for CppTypes

---
+++ Il2CppTests/TestCppTypes.cs
@@ -0,0 +1,28 @@
+﻿/*
+    Copyright 2020 Katy Coe - http://www.hearthcode.org - http://www.djkaty.com
+
+    All rights reserved.
+*/
+
+using System;
+using System.IO;
+using Il2CppInspector.Cpp;
+using Il2CppInspector.Outputs.UnityHeaders;
+using Il2CppInspector.Reflection;
+using NUnit.Framework;
+
+namespace Il2CppInspector
+{
+    [TestFixture]
+    public partial class FixedTests 
+    {
+        [Test]
+        public void TestCppTypes() {
+            // TODO: Flesh out CppTypes test
+
+            var cppTypes = CppTypes.FromUnityHeaders(new UnityVersion("2019.3.1f1"));
+
+            throw new NotImplementedException();
+        }
+    }
+}
