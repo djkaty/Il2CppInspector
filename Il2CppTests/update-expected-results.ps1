64fbee92b0a5163fa1c5e8bf6ec9b05d4c1ff554
Katy Coe <djkaty@users.noreply.github.com>
2020-07-20 20:18:45 +0200

    Tests: Fix copy il2cpp-types.h from wrong location in update-expected-results.ps1

--- Il2CppTests/update-expected-results.ps1
+++ Il2CppTests/update-expected-results.ps1
@@ -10,8 +10,8 @@ $bin = (gci "$PSScriptRoot/TestBinaries/*/*" -Filter test-result.cs)
 $bin2 = (gci "$PSScriptRoot/TestBinaries/*/*" -Filter test-result-1.cs)
 $py = (gci "$PSScriptRoot/TestBinaries/*/*" -Filter test-ida-result.py)
 $py2 = (gci "$PSScriptRoot/TestBinaries/*/*" -Filter test-ida-result-1.py)
-$cpp = (gci "$PSScriptRoot/TestBinaries/*/*" -Filter test-result.h)
-$cpp2 = (gci "$PSScriptRoot/TestBinaries/*/*" -Filter test-result-1.h)
+$cpp = (gci "$PSScriptRoot/TestBinaries/*/test-cpp-result/*" -Filter il2cpp-types.h)
+$cpp2 = (gci "$PSScriptRoot/TestBinaries/*/test-cpp-result-1/*" -Filter il2cpp-types.h)
 
 # Get path to expected test results
 $results = "$PSScriptRoot/TestExpectedResults"
@@ -37,11 +37,11 @@ $py2 | % {
 }
 
 $cpp | % {
-	$target = $results + "/" + (Split-Path -Path (Split-Path -Path $_) -Leaf) + ".h"
+	$target = $results + "/" + (Split-Path -Path (Split-Path -Path (Split-Path -Path $_)) -Leaf) + ".h"
 	cp $_ -Destination $target -Force
 }
 
 $cpp2 | % {
-	$target = $results + "/" + (Split-Path -Path (Split-Path -Path $_) -Leaf) + "-1.h"
+	$target = $results + "/" + (Split-Path -Path (Split-Path -Path (Split-Path -Path $_)) -Leaf) + "-1.h"
 	cp $_ -Destination $target -Force
 }
