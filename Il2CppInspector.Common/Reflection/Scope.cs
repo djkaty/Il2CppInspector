3fb0b085daffc3963ad12f2e837e1adb0e3dbcf5
Katy Coe <djkaty@users.noreply.github.com>
2020-08-09 19:24:54 +0200

    Model: Add Scope.Empty static helper

--- Il2CppInspector.Common/Reflection/Scope.cs
+++ Il2CppInspector.Common/Reflection/Scope.cs
@@ -11,6 +11,9 @@ namespace Il2CppInspector.Reflection
     // A code scope with which to evaluate how to output type references
     public class Scope
     {
+        // A scope at the root level with no available namespaces (guarantees full-name retrieval for any type)
+        public static Scope Empty = new Scope();
+
         // The scope we are currently in
         public TypeInfo Current;
 
