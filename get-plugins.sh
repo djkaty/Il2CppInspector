4fe874978a7ebe9e311f261a3a62b74bead68399
Katy Coe <djkaty@users.noreply.github.com>
2020-12-19 21:57:57 +0100

    Plugins: Add get-plugins.sh script

---
+++ get-plugins.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Copyright 2020 Katy Coe - http://www.djkaty.com - https://github.com/djkaty
+# All rights reserved.
+
+# Download the latest set of plugins to a temporary file, extract the archive then delete it
+wget https://github.com/djkaty/Il2CppInspectorPlugins/releases/latest/download/plugins.zip
+unzip plugins.zip
+rm plugins.zip
+
