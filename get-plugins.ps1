b94d8a4d9230293bf0669dd73962795fa21b08e0
Katy Coe <djkaty@users.noreply.github.com>
2020-12-19 21:37:08 +0100

    Plugins: Add get-plugins.ps1 script

---
+++ get-plugins.ps1
@@ -0,0 +1,8 @@
+# Copyright 2020 Katy Coe - http://www.djkaty.com - https://github.com/djkaty
+# All rights reserved.
+
+# Download the latest set of plugins to a temporary file, extract the archive then delete it
+$temp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } –PassThru
+wget -OutFile $temp https://github.com/djkaty/Il2CppInspectorPlugins/releases/latest/download/plugins.zip
+Expand-Archive -Path $temp -DestinationPath $pwd -Force
+del $temp
